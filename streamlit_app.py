49772758ef089f5c25e606d0fefd9033c2cdfa9e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-20 10:18:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 with st.sidebar:
-   st.title('🤖 Streamlit for Generative AI course')
+   st.title('Streamlit for Generative AI course')
    st.write('The Streamlit for Generative AI course will show you how to use Streamlit to build large language model (LLM) powered apps.')
+
+
 
