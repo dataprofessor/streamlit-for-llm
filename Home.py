a290da44e6a07345af8f54669d3ef6fbcef25a41
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-20 21:58:18 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,6 +16,8 @@ st.markdown('''
 </p>
 ''', unsafe_allow_html=True)
 
+st.metric(label="Active developers", value=123, delta=None)
+
 st.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.')
 
 load_css()
