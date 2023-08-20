f3f47f1e42775aba50efdd1ead8a8067196d9db7
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-20 10:19:50 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from utilities import load_css
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="🏠",
+)
+
+st.header("Welcome! 👋")
+st.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.')
+
+load_css()
