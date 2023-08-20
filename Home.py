8820a8448228c86dd0956535f52cc49fd68978ba
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-20 22:24:05 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,6 +14,17 @@ st.image(img)
 st.header('About')
 st.write('The Streamlit for Generative AI course will show you how to use Streamlit to build large language model (LLM) powered apps. Finally you can deploy the Streamlit app to the cloud and share with the community.')
 
-
+# Table of Contents
+st.header('Table of Contents')
+st.markdown('''
+- [**Lesson 1** - Getting up to speed with Streamlit](Apples)
+- [**Lesson 2** - Using LLM models from OpenAI](./content/Lesson-2.md)
+- [**Lesson 3** - Using open source LLM models from Hugging Face Hub](./content/Lesson-3.md)
+- [**Lesson 4** - Using hosted open source LLM models from Replicate](./content/Lesson-4.md)
+- [**Lesson 5** - Orchestrating an LLM workflow with LangChain](./content/Lesson-5.md)
+- [**Project 1** - Build a ChatGPT clone](./content/Project-1.md)
+- [**Project 2** - Build a Llama 2 chatbot](./content/Project-2.md)
+- [**Project 3** - Build a HugChat chatbot](./content/Project-3.md)
+''')
 
 load_css()
