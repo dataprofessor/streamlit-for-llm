1a41ca08208848a99b475d9a4269213a98bf588f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-20 21:10:24 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,12 @@ st.set_page_config(
     page_icon="🏠",
 )
 
-st.header("Welcome! 👋")
+st.markdown('''
+<p align="center">
+  <img src="./img/streamlit-generative-ai-course-logo.png" width="60%">
+</p>
+''', unsafe_allow_html=True)
+
 st.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.')
 
 load_css()
