7539b1f23ec309cd081c01d079032842967d9975
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-20 22:19:56 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,8 +10,8 @@ st.set_page_config(
 img = Image.open('img/streamlit-generative-ai-course-logo.png')
 st.image(img)
 
+# About
 st.header('About')
-
 st.write('The Streamlit for Generative AI course will show you how to use Streamlit to build large language model (LLM) powered apps. Finally you can deploy the Streamlit app to the cloud and share with the community.')
 
 
