34ddc89447285a9060aff72380cda13ee4845e49
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-21 13:33:51 +0700

    Update 3_3️⃣_Lesson_3_Hugging_Face_Hub.py

--- pages/3_3️⃣_Lesson_3_Hugging_Face_Hub.py
+++ pages/3_3️⃣_Lesson_3_Hugging_Face_Hub.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Using open source LLM models from Hugging Face Hub", page_icon="3️⃣")
+st.set_page_config(page_title="Lesson 3 - Using open source LLM models from Hugging Face Hub", page_icon="3️⃣")
 
-st.header("3️⃣ Using open source LLM models from Hugging Face Hub")
+st.header("3️⃣ Lesson 3 - Using open source LLM models from Hugging Face Hub")
 
 st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
