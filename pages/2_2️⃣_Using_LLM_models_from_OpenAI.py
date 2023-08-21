ce66e8544f82fb0f0facfe8a5770ef40f96a0ce3
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-21 12:55:22 +0700

    Update and rename 2_2️⃣_Oranges.py to 2_2️⃣_Using_LLM_models_from_OpenAI.py

--- pages/2_2️⃣_Using_LLM_models_from_OpenAI.py
+++ pages/2_2️⃣_Using_LLM_models_from_OpenAI.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Oranges", page_icon="2️⃣")
+st.set_page_config(page_title="Using LLM models from OpenAI", page_icon="2️⃣")
 
-st.header("2️⃣ Oranges 🍊")
+st.header("2️⃣ Using LLM models from OpenAI")
 
 st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
