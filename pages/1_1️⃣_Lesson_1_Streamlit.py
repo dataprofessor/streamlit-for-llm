fdb6da9b29a6e44c200cb77f5a14611061c35cf4
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-21 13:34:17 +0700

    Update 1_1️⃣_Lesson_1_Streamlit.py

--- pages/1_1️⃣_Lesson_1_Streamlit.py
+++ pages/1_1️⃣_Lesson_1_Streamlit.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Getting up to speed with Streamlit", page_icon="1️⃣")
+st.set_page_config(page_title="Lesson 1 - Getting up to speed with Streamlit", page_icon="1️⃣")
 
-st.header("1️⃣ Getting up to speed with Streamlit")
+st.header("1️⃣ Lesson 1 - Getting up to speed with Streamlit")
 
 st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
