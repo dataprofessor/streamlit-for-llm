e4ffa14015b543d4f3bd2d9e5bc3e02e7fc59041
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-21 12:53:35 +0700

    Update 1_1️⃣_Apples.py

--- pages/1_1️⃣_Apples.py
+++ pages/1_1️⃣_Apples.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Apples", page_icon="1️⃣")
+st.set_page_config(page_title="Getting up to speed with Streamlit", page_icon="1️⃣")
 
-st.header("1️⃣ Apples 🍎")
+st.header("1️⃣ Getting up to speed with Streamlit")
 
 st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
