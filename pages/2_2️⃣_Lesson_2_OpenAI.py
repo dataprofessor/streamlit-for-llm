d84baa3d2f813e966a989cb04d1c8af23ef6c5c0
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-21 13:34:04 +0700

    Update 2_2️⃣_Lesson_2_OpenAI.py

--- pages/2_2️⃣_Lesson_2_OpenAI.py
+++ pages/2_2️⃣_Lesson_2_OpenAI.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Using LLM models from OpenAI", page_icon="2️⃣")
+st.set_page_config(page_title="Lesson 2 - Using LLM models from OpenAI", page_icon="2️⃣")
 
-st.header("2️⃣ Using LLM models from OpenAI")
+st.header("2️⃣ Lesson 2 - Using LLM models from OpenAI")
 
 st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
