b2ce819b475e4a3b4fe6c845097ea29005ac54a3
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-21 13:32:36 +0700

    Update 7_2️⃣_Project_2_Llama_2_chatbot.py

--- pages/7_2️⃣_Project_2_Llama_2_chatbot.py
+++ pages/7_2️⃣_Project_2_Llama_2_chatbot.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Build a Llama 2 chatbot", page_icon="2️⃣")
+st.set_page_config(page_title="Project 2 - Build a Llama 2 chatbot", page_icon="2️⃣")
 
-st.header("2️⃣ Build a Llama 2 chatbot")
+st.header("2️⃣ Project 2 - Build a Llama 2 chatbot")
 
 st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
