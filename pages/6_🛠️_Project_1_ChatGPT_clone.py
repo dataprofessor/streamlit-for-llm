9f79871cc2e4efbe363f6e505a414662ada3bc26
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-23 00:31:38 +0700

    Update and rename 6_1️⃣_Project_1_ChatGPT_clone.py to 6_🛠️_Project_1_ChatGPT_clone.py

--- pages/6_🛠️_Project_1_ChatGPT_clone.py
+++ pages/6_🛠️_Project_1_ChatGPT_clone.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Project 1 - Build a ChatGPT clone", page_icon="1️⃣")
+st.set_page_config(page_title="Project 1 - Build a ChatGPT clone", page_icon="🛠️")
 
-st.header("1️⃣ Project 1 - Build a ChatGPT clone")
+st.header("🛠️ Project 1 - Build a ChatGPT clone")
 
 st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
