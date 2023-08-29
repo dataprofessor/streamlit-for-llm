c195a83f3d43b08335d29c9d802cf0234cad13ee
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-29 15:08:58 +0700

    Update 2_📖_Lesson_2_OpenAI.py

--- pages/2_📖_Lesson_2_OpenAI.py
+++ pages/2_📖_Lesson_2_OpenAI.py
@@ -164,7 +164,11 @@ completion = openai.ChatCompletion.create(
   # Use GPT 3.5 as the LLM
   model="gpt-3.5-turbo",
   # Pre-define conversation messages for the possible roles 
-
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+   ]
+ )
 ```
 
 
