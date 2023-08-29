eefaaa4e7bd07a0c61dcdf2294a760f5e94f5166
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-29 15:07:04 +0700

    Update 2_📖_Lesson_2_OpenAI.py

--- pages/2_📖_Lesson_2_OpenAI.py
+++ pages/2_📖_Lesson_2_OpenAI.py
@@ -159,6 +159,16 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 # Define the user prompt message
 prompt = "Hello!"
+# Create a chatbot using ChatCompletion.create() function
+completion = openai.ChatCompletion.create(
+  # Use GPT 3.5 as the LLM
+  model="gpt-3.5-turbo",
+  # Pre-define conversation messages for the possible roles 
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+  ]
+)
 ```
 
 
