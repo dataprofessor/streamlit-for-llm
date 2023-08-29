8bd33122feced5b3421a6cc812934e617aaa91a1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-29 15:03:51 +0700

    Update 2_📖_Lesson_2_OpenAI.py

--- pages/2_📖_Lesson_2_OpenAI.py
+++ pages/2_📖_Lesson_2_OpenAI.py
@@ -150,28 +150,6 @@ As already mentioned above, going forward the chat completions API will be used 
 ### 8.1. Test
 Let us now proceed to using the Chat Completions API by providing it with an input prompt, and in this example, we use **_Hello_**!
 
-```
-# Import prerequisite libraries
-import os
-import openai
-
-# Setting the API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
-# Define the user prompt message
-prompt = "Hello!"
-# Create a chatbot using ChatCompletion.create() function
-completion = openai.ChatCompletion.create(
-  # Use GPT 3.5 as the LLM
-  model="gpt-3.5-turbo",
-  # Pre-define conversation messages for the possible roles 
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt}
-  ]
-)
-# Print the returned output from the LLM model
-print(completion.choices[0].message)
-```
 
 
 
