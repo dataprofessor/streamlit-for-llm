5fa8e4bbf40a24665c47bb54e0a4c5190f2ed9e8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-30 18:51:44 +0700

    Update 3_📖_Lesson_3_Hugging_Face_Hub.py

--- pages/3_📖_Lesson_3_Hugging_Face_Hub.py
+++ pages/3_📖_Lesson_3_Hugging_Face_Hub.py
@@ -1,19 +1,22 @@
 import streamlit as st
 from utilities import load_css
 
+
 st.set_page_config(page_title="Lesson 3 - Using open source LLM models from Hugging Face Hub", page_icon="📖")
 
 st.header("📖 Lesson 3 - Using open source LLM models from Hugging Face Hub")
 
+#####
+import requests
 
 img_url_path = 'https://github.com/dataprofessor/streamlit-for-generative-ai/blob/master'
 img_url_suffix = '?raw=true'
 
 md_url_path = 'https://raw.githubusercontent.com/dataprofessor/streamlit-for-generative-ai/master/content'
 md_lesson_url = f'{md_url_path}/Lesson-3.md'
+response = requests.get(md_lesson_url)
+content = response.text
 
-with open(md_lesson_url, 'r') as f:
-  content = f.readlines()
-  st.markdown(content, unsafe_allow_html=True)
+st.markdown(content, unsafe_allow_html=True)
 
 load_css()
