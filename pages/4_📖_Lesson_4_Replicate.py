198132d4116c943917a2aa1b4d268bd6c1f28879
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-30 20:12:30 +0700

    Update 4_📖_Lesson_4_Replicate.py

--- pages/4_📖_Lesson_4_Replicate.py
+++ pages/4_📖_Lesson_4_Replicate.py
@@ -4,5 +4,6 @@ from utilities import load_css, read_md
 st.set_page_config(page_title="Lesson 4 - Using hosted open source LLM models from Replicate", page_icon="📖")
 
 load_css()
+
 md = read_md('Lesson-4.md')
 st.markdown(md, unsafe_allow_html=True)
