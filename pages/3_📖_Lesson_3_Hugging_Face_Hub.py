9a4614af0fa80624351bbecedd67ac297fee9ec8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-30 20:10:12 +0700

    Update 3_📖_Lesson_3_Hugging_Face_Hub.py

--- pages/3_📖_Lesson_3_Hugging_Face_Hub.py
+++ pages/3_📖_Lesson_3_Hugging_Face_Hub.py
@@ -4,7 +4,6 @@ import requests
 
 st.set_page_config(page_title="Lesson 3 - Using open source LLM models from Hugging Face Hub", page_icon="📖")
 
+load_css()
 md = read_md('Lesson-3.md')
 st.markdown(md, unsafe_allow_html=True)
-
-load_css()
