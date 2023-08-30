12ed834aa5a1c61b04db24f6a21ebb5e54f61dcc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-30 20:12:16 +0700

    Update 5_📖_Lesson_5_LangChain.py

--- pages/5_📖_Lesson_5_LangChain.py
+++ pages/5_📖_Lesson_5_LangChain.py
@@ -4,5 +4,6 @@ from utilities import load_css, read_md
 st.set_page_config(page_title="Lesson 5 - Orchestrating an LLM workflow with LangChain", page_icon="📖")
 
 load_css()
+
 md = read_md('Lesson-5.md')
 st.markdown(md, unsafe_allow_html=True)
