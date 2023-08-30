7ef1fe55110265f10ef37732038c2064f4c9e73e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-30 20:04:38 +0700

    Update 3_📖_Lesson_3_Hugging_Face_Hub.py

--- pages/3_📖_Lesson_3_Hugging_Face_Hub.py
+++ pages/3_📖_Lesson_3_Hugging_Face_Hub.py
@@ -4,21 +4,22 @@ import requests
 
 st.set_page_config(page_title="Lesson 3 - Using open source LLM models from Hugging Face Hub", page_icon="📖")
 
-def read_md(md_file):
-  img_url_path = 'https://github.com/dataprofessor/streamlit-for-generative-ai/blob/master'
-  img_url_suffix = '?raw=true'
+#def read_md(md_file):
+img_url_path = 'https://github.com/dataprofessor/streamlit-for-generative-ai/blob/master'
+img_url_suffix = '?raw=true'
   
-  md_url_path = 'https://raw.githubusercontent.com/dataprofessor/streamlit-for-generative-ai/master/content'
-  md_lesson_url = f'{md_url_path}/{md_file}.md'
-  response = requests.get(md_lesson_url)
-  content = response.text
-  content = content.replace('<img src="..', f'<img src="{img_url_path}').replace('" width', f'{img_url_suffix}" width')
-  return content
+md_url_path = 'https://raw.githubusercontent.com/dataprofessor/streamlit-for-generative-ai/master/content'
+md_lesson_url = f'{md_url_path}/{md_file}.md'
+response = requests.get(md_lesson_url)
+content = response.text
+content = content.replace('<img src="..', f'<img src="{img_url_path}').replace('" width', f'{img_url_suffix}" width')
 
-md = read_md('Lesson-3.md')
+st.markdown(content, unsafe_allow_html=True)
 
-md
-
+#md = read_md('Lesson-3.md')
 #st.markdown(md, unsafe_allow_html=True)
+
+
+
 
 load_css()
