c165178ea3882362888ee6eb34eff026660eac74
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-30 20:07:49 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
-from utilities import load_css, read_md
-import requests
+from utilities import load_css
 
 st.set_page_config(
     page_title="Welcome to the Streamlit for Generative AI course",
