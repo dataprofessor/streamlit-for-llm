0663123a633eae3cdd99f47baedeb184a31080d6
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-30 20:06:49 +0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -10,7 +10,7 @@ def read_md(md_file):
   img_url_suffix = '?raw=true'
   
   md_url_path = 'https://raw.githubusercontent.com/dataprofessor/streamlit-for-generative-ai/master/content'
-  md_lesson_url = f'{md_url_path}/{md_file}.md'
+  md_lesson_url = f'{md_url_path}/{md_file}'
   response = requests.get(md_lesson_url)
   content = response.text
   content = content.replace('<img src="..', f'<img src="{img_url_path}').replace('" width', f'{img_url_suffix}" width')
