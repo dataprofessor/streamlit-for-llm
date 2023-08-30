3d65d9bfd29d2ce88f22e9e6a02c8e7de806f764
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-30 19:28:43 +0700

    Update 3_📖_Lesson_3_Hugging_Face_Hub.py

--- pages/3_📖_Lesson_3_Hugging_Face_Hub.py
+++ pages/3_📖_Lesson_3_Hugging_Face_Hub.py
@@ -1,22 +1,9 @@
 import streamlit as st
-from utilities import load_css
-
-
-st.set_page_config(page_title="Lesson 3 - Using open source LLM models from Hugging Face Hub", page_icon="📖")
-#st.header("📖 Lesson 3 - Using open source LLM models from Hugging Face Hub")
-
-#####
-import requests
-
-img_url_path = 'https://github.com/dataprofessor/streamlit-for-generative-ai/blob/master'
-img_url_suffix = '?raw=true'
-
-md_url_path = 'https://raw.githubusercontent.com/dataprofessor/streamlit-for-generative-ai/master/content'
-md_lesson_url = f'{md_url_path}/Lesson-3.md'
-response = requests.get(md_lesson_url)
-content = response.text
-content = content.replace('<img src="..', f'<img src="{img_url_path}').replace('" width', f'{img_url_suffix}" width')
-
-st.markdown(content, unsafe_allow_html=True)
+from utilities import load_css, read_md
 
 load_css()
+
+st.set_page_config(page_title="Lesson 3 - Using open source LLM models from Hugging Face Hub", page_icon="📖")
+
+md = read_md('Lesson-3.md')
+st.markdown(md, unsafe_allow_html=True)
