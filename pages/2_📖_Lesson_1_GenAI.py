c4a7b8993eee6e2a2adc652bab554055903ec8a2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-30 20:21:47 +0700

    Create 2_📖_Lesson_1_GenAI.py

---
+++ pages/2_📖_Lesson_1_GenAI.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from utilities import load_css, read_md
+
+st.set_page_config(page_title="Lesson 2 - Using LLM models from OpenAI", page_icon="📖")
+
+load_css()
+
+md = read_md('Lesson-2.md')
+st.markdown(md, unsafe_allow_html=True)
