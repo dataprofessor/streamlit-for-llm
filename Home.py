768422b60947e6d154e63574f5d8ba0e062526e1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-02 09:00:57 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -93,6 +93,8 @@ with st.expander('Testing ...'):
         switch_page('Lesson_1_Generative_AI')
 
 
+
+st.markdown('<a href="?page=Lesson_1_Generative_AI" target="_top"> <b>Lesson 1</b> - An introduction to Generative AI </a>', unsafe_allow_html=True)
 params = st.experimental_get_query_params()
 if 'page' in params.keys():
     switch_page(params.get('page')[0])
