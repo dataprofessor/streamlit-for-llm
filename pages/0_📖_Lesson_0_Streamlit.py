c98547981d434b99c9390ba8a38c09a998371669
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-04 12:45:46 +0700

    Update 0_📖_Lesson_0_Streamlit.py

--- pages/0_📖_Lesson_0_Streamlit.py
+++ pages/0_📖_Lesson_0_Streamlit.py
@@ -149,22 +149,21 @@ with st.expander('See st.selectbox() example'):
 
 st.subheader('Chat elements')
 
-with st.expander('See st.chat_input() example', expanded=True):
-  st.subheader('st.chat_input()')
-  st.text('Display a chat input widget.')
-  col1, col2 = st.columns((3,2))
-  with col1:
-    st.markdown('**Code**')
-    st.code('''
-      import streamlit as st
+st.subheader('st.chat_input()')
+st.text('Display a chat input widget.')
+col1, col2 = st.columns((3,2))
+with col1:
+  st.markdown('**Code**')
+  st.code('''
+    import streamlit as st
   
-      prompt = st.chat_input("Say something")
-      if prompt:
-        st.write(f"User has sent the following prompt: {prompt}")
-    ''')
-  with col2:
-    st.markdown('**App**')
     prompt = st.chat_input("Say something")
     if prompt:
       st.write(f"User has sent the following prompt: {prompt}")
-  st.markdown('[More info on the Docs page](https://docs.streamlit.io/library/api-reference/chat/st.chat_input)')
+    ''')
+with col2:
+  st.markdown('**App**')
+  prompt = st.chat_input("Say something")
+  if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+st.markdown('[More info on the Docs page](https://docs.streamlit.io/library/api-reference/chat/st.chat_input)')
