0adf4a4ebbdd1cc24da5c6016f06e53d1b788197
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-04 18:59:02 +0700

    Update 5_🛠️_Project_0_Streamlit_Chat_elements.py

--- pages/5_🛠️_Project_0_Streamlit_Chat_elements.py
+++ pages/5_🛠️_Project_0_Streamlit_Chat_elements.py
@@ -170,6 +170,23 @@ st.header('Code explanation of the Echo bot', divider='red')
 st.markdown("""
 The above code snippet allows us to build a bot that mirrors or echoes your input. More specifically, the bot will respond to your input with the same message. We'll use `st.chat_message` to display the user's input and `st.chat_input` to accept user input. We'll also use session state to store the chat history so we can display it in the chat message container.
 
-
-
+First, let's think about the different components we'll need to build our bot:
+- Two chat message containers to display messages from the user and the bot, respectively.
+- A chat input widget so the user can type in a message.
+- A way to store the chat history so we can display it in the chat message containers. We can use a list to store the messages, and append to it every time the user or bot sends a message. Each entry in the list will be a dictionary with the following keys: role (the author of the message), and content (the message content).
 """)
+
+st.code('''
+import streamlit as st
+
+st.title("Echo Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+''')
