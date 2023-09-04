01c973098629f0c48d0f8ea85570953d4061c3e2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-04 09:04:39 +0700

    Update 0_📖_Lesson_0_Streamlit.py

--- pages/0_📖_Lesson_0_Streamlit.py
+++ pages/0_📖_Lesson_0_Streamlit.py
@@ -14,6 +14,7 @@ st.header('Examples')
 st.subheader('st.text_input()')
 col1, col2 = st.columns(2)
 with col1:
+  st.markdown('### Code')
   st.code('''
     import streamlit as st
 
@@ -21,5 +22,6 @@ with col1:
     st.write('The current movie title is', title)
   ''')
 with col2:
+  st.markdown('### App')
   title = st.text_input('Movie title', 'Life of Brian')
   st.write('The current movie title is', title)
