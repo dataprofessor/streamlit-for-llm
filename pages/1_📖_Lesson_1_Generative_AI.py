09f03b39f0b3adc96268a5d206abace911162af8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-04 19:26:04 +0700

    Update 1_📖_Lesson_1_Generative_AI.py

--- pages/1_📖_Lesson_1_Generative_AI.py
+++ pages/1_📖_Lesson_1_Generative_AI.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utilities import load_css, read_md
 
-st.set_page_config(page_title="Lesson 1 - An introduction to Generative AI", page_icon="📖")
+st.set_page_config(page_title="Lesson 1 - An introduction to Generative AI", page_icon="📖", layout="wide")
 
 load_css()
 
