696ce276bfa2e03ef65304e038ea7c8fd70b9710
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-04 14:03:05 +0700

    Update 0_📖_Lesson_0_Streamlit.py

--- pages/0_📖_Lesson_0_Streamlit.py
+++ pages/0_📖_Lesson_0_Streamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import time
 from utilities import load_css, read_md
 
 st.set_page_config(page_title="Lesson 0 - Getting up to speed with Streamlit", page_icon="📖", layout="wide")
@@ -148,79 +147,3 @@ with st.expander('See st.selectbox() example'):
     st.write('You selected:', option)
   st.markdown('[More info on the Docs page](https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)')
 
-
-st.subheader('Chat elements')
-
-with st.expander('See st.chat_message() example', expanded=True):
-  st.subheader('st.chat_message()')
-  st.text('Insert a chat message container.')
-  col1, col2 = st.columns((3,2))
-  with col1:
-    st.markdown('**Code**')
-    st.code('''
-      import streamlit as st
-      import numpy as np
-
-      with st.chat_message("user"):
-        st.write("Hello 👋")
-        st.line_chart(np.random.randn(30, 3))
-    ''')
-  with col2:
-    st.markdown('**App**')
-    with st.chat_message("user"):
-      st.write("Hello 👋")
-      st.line_chart(np.random.randn(30, 3))
-  st.markdown('[More info on the Docs page](https://docs.streamlit.io/library/api-reference/chat/st.chat_message)')
-
-
-st.subheader('st.status()')
-st.text('Insert a status container to display output from long-running tasks.')
-col1, col2 = st.columns((3,2))
-with col1:
-    st.markdown('**Code**')
-    st.code('''
-      import streamlit as st
-      import time
-
-      if st.button('Start'):
-        with st.status("Downloading data..."):
-          st.write("Searching for data...")
-          time.sleep(2)
-          st.write("Found URL.")
-          time.sleep(1)
-          st.write("Downloading data...")
-          time.sleep(1)
-    ''')
-with col2:
-    st.markdown('**App**')
-    if st.button('Start'):
-      with st.status("Downloading data..."):
-        st.write("Searching for data...")
-        time.sleep(2)
-        st.write("Found URL.")
-        time.sleep(1)
-        st.write("Downloading data...")
-        time.sleep(1)
-st.markdown('[More info on the Docs page](https://docs.streamlit.io/library/api-reference/chat/st.chat_message)')
-
-
-
-st.subheader('st.chat_input()')
-st.text('Display a chat input widget.')
-
-st.markdown('**Code**')
-st.code('''
-  import streamlit as st
-  
-  prompt = st.chat_input("Say something")
-  if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
-  ''')
-
-st.markdown('**App**')
-prompt = st.chat_input("Say something")
-
-if prompt:
-  st.write(f"User has sent the following prompt: {prompt}")
-
-st.markdown('[More info on the Docs page](https://docs.streamlit.io/library/api-reference/chat/st.chat_input)')
