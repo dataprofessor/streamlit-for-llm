715a10583b13e2ca04a36469ba5412ac38fb3b23
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-04 19:26:21 +0700

    Update 2_📖_Lesson_2_OpenAI.py

--- pages/2_📖_Lesson_2_OpenAI.py
+++ pages/2_📖_Lesson_2_OpenAI.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utilities import load_css, read_md
 
-st.set_page_config(page_title="Lesson 2 - Using LLM models from OpenAI", page_icon="📖")
+st.set_page_config(page_title="Lesson 2 - Using LLM models from OpenAI", page_icon="📖", layout="wide")
 
 load_css()
 
