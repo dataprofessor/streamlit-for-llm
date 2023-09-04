9d020f12d47c6222f4310684c8740abeb0544329
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-04 19:26:38 +0700

    Update 4_📖_Lesson_4_Replicate.py

--- pages/4_📖_Lesson_4_Replicate.py
+++ pages/4_📖_Lesson_4_Replicate.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utilities import load_css, read_md
 
-st.set_page_config(page_title="Lesson 4 - Using hosted open source LLM models from Replicate", page_icon="📖")
+st.set_page_config(page_title="Lesson 4 - Using hosted open source LLM models from Replicate", page_icon="📖", layout="wide")
 
 load_css()
 
