d095097dbc16ec8737bd5849c38c045ede2b51be
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-04 14:19:02 +0700

    Update 5_🛠️_Project_0_Streamlit_Chat_elements.py

--- pages/5_🛠️_Project_0_Streamlit_Chat_elements.py
+++ pages/5_🛠️_Project_0_Streamlit_Chat_elements.py
@@ -104,7 +104,7 @@ st.markdown('''
     This example app is from the [Build conversational apps](https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps#build-a-bot-that-mirrors-your-input) article.
 ''')
 
-code = '''
+st.code('''
 import streamlit as st
 
 st.title("Echo Bot")
@@ -131,10 +131,34 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
-'''
-
-st.code(code)
+''')
 
 def echo_bot():
-    return code
+    import streamlit as st
+
+    st.title("Echo Bot")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+    # React to user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+    
+        response = f"Echo: {prompt}"
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+    
 sandbox(echo_bot)
